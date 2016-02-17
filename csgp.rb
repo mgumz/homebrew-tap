efce17183b9a11ba57d51859ef1079070252bc56
Mathias Gumz <mg@2hoch5.com>
2016-02-17 21:00:30 +0100

    bump *csgp* to v1.0.1

--- csgp.rb
+++ csgp.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Csgp < Formula
-  version   '1.0.0'
+  version   '1.0.1'
   homepage  'https://github.com/mgumz/csgp'
-  url       'https://github.com/mgumz/csgp/archive/v1.0.0.tar.gz'
-  sha256    'c73cd158c06b308ff9eb5063f8f039e1fbcfa74c662ab3efa7591d33de4c28c9'
+  url       'https://github.com/mgumz/csgp/archive/v1.0.1.tar.gz'
+  sha256    '3a1c133b07428d647b2ae7acb99a16c31de032543072d9e74b910d0c82e7fffa'
 
-  head      'git@github.com:mgumz/csgp.git', :revision => '0cb7ae63cc9b1090227425d7f84446bf59efca4c'
+  head      'git@github.com:mgumz/csgp.git', :revision => '1f80dc10469ed56daa915ab4685de54f141edadc'
 
   def install
     system "make"
